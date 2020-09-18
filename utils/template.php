f36701ec5a5a1fb048c9279808a15b97cf7592e2
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-09-18 20:33:17 +0200

    Adaptation of the project structure

---
+++ utils/template.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wicodus - Responsive HTML5 Template</title>
+
+    <!-- CSS -->
+    <link href="assets/css/fonts/etline-font.min.css" rel="stylesheet">
+    <link href="assets/css/fonts/fontawesome/all.min.css" rel="stylesheet">
+    <link href="assets/css/fonts/pe-icon-7-stroke.css" rel="stylesheet">
+    <link href="assets/css/fonts/themify-icons.css" rel="stylesheet">
+
+    <link href="assets/plugins/owl.carousel/owl.carousel.min.css" rel="stylesheet">
+
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" href="assets/img/favicon.png">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="page-body">
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top border-nav">
+      <div class="container">
+        <a class="navbar-brand align-items-center logo" href="#">
+          <img src="assets/img/logo-gaming.png" alt="Wicodus" class="logo-light mx-auto">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">☰</button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="nav navbar-nav ml-lg-auto">
+            <li class="nav-item"><a class="nav-link mr-2" href="" data-scroll="#about">Game</a></li>
+            <li class="nav-item"><a class="nav-link mr-2" href="" data-scroll="#characters">Characters</a></li>
+            <li class="nav-item"><a class="nav-link mr-2" href="" data-scroll="#overview">Overview</a></li>
+            <li class="nav-item"><a class="nav-link mr-2" href="" data-scroll="#gallery">Gallery</a></li>
+            <li class="nav-item"><a class="nav-link mr-2" href="" data-scroll="#contact">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- header -->
+    <header id="header" class="header h-fullscreen text-light">
+      <div class="media-container parallax-window" data-parallax="scroll" data-image-src="assets/img/content/game-bg.jpg"></div>
+      <div class="overlay pe-n bg-dark_A-40"></div>
+      <div class="overlay d-flex align-items-center">
+        <div class="container text-center">
+          <div class="row align-items-center">
+            <div class="col-lg-10 mx-auto">
+              <div>
+                <img src="assets/img/content/game-logo.png" alt="Game Logo">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fadeIn ad-800ms">
+        <div class="down-arrow floating-arrow absolute-center-X">
+          <span class="fas fa-chevron-down"></span>
+        </div>
+      </div>
+    </header>
+    <!-- /.header -->
+
+    <!-- Start Main Content -->
+    <main class="main-content">
+
+      <!-- content area -->
+      <section id="about" class="content-section position-relative text-light text-center parallax-window" data-parallax="scroll" data-image-src="assets/img/content/bg-2.jpg" data-overlay="8">
+        <div class="container position-relative">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div>
+                <h2 class="ls-1">About</h2>
+                <hr class="w-10 border-warning border-top-2 o-90">
+                <p class="lead-1">Quisque sit amet erat posuere scelerisque. Quisque fermentum eget arcu aliquet tempor. Phasellus sit amet bibendum diam, sed vulputate magna. Proin ante dolor, ultricies ac laoreet sed, fermentum sit amet nulla. Suspendisse bibendum dictum tempus. Aliquam turpis mauris, sagittis vitae egestas a, scelerisque. Donec justo turpis, porta a urna vitae, convallis lobortis risus. Integer porta malesuada tristique. Nullam aliquet mattis sollicitudin.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content area -->
+
+      <!-- content area -->
+      <section id="characters" class="characters content-section latest-articles text-light">
+        <div class="container">
+          <header class="section-header text-center">
+            <h2>Characters</h2>
+            <hr class="w-10 border-warning border-top-2 o-90">
+          </header>
+          <div class="row">
+            <div class="col-sm-7 col-md-6 col-lg-3 mx-auto mb-7 md-lg-0">
+              <article class="card article-post bg-transparent">
+                <img class="article-image rounded mb-4" src="assets/img/content/characters/1.jpg" alt="characters">
+                <figure class="border border-secondary p-4">
+                  <figcaption>
+                    <h5 class="mt-1 text-unset"><a href="#">Praesent</a></h5>
+                    <hr class="my-4 border-secondary">
+                    <q>Donec consectetur, nisi et ullamcorper mollis, magna augue ultrices tortor, nec malesuada quam est.</q>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+            <div class="col-sm-7 col-md-6 col-lg-3 mx-auto mb-7 md-lg-0">
+              <article class="card article-post bg-transparent">
+                <img class="article-image rounded mb-4" src="assets/img/content/characters/2.jpg" alt="characters">
+                <figure class="border border-secondary p-4">
+                  <figcaption>
+                    <h5 class="mt-1 text-unset"><a href="#">Nullam</a></h5>
+                    <hr class="my-4 border-secondary">
+                    <q>Donec consectetur, nisi et ullamcorper mollis, magna augue ultrices tortor, nec malesuada quam est.</q>
+                  </figcaption>
+                </figure>
+              </article>
+            </div>
+            <div class="col-sm-7 col-md-6 col-lg-3 mx-auto mb-7 mb-md-0">
+              <article class="card article-post bg-transparent">
+                <img class="article-image rounded mb-4" src="assets/img/content/characters/3.jpg" alt="characters">
+                <figure class="border border-secondary p-4">
+                  <figcaption>
+                    <h5 class="mt-1 text-unset"><a href="#">Suspendisse</a></h5>
+                    <hr class="my-4 border-secondary">
+                    <q>Donec consectetur, nisi et ullamcorper mollis, magna augue ultrices tortor, nec malesuada quam est.</q>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+            <div class="col-sm-7 col-md-6 col-lg-3 mx-auto mb-7 mb-md-0">
+              <article class="card article-post bg-transparent">
+                <img class="article-image rounded mb-4" src="assets/img/content/characters/4.jpg" alt="characters">
+                <figure class="border border-secondary p-4">
+                  <figcaption>
+                    <h5 class="mt-1 text-unset"><a href="#">Curabitur</a></h5>
+                    <hr class="my-4 border-secondary">
+                    <q>Donec consectetur, nisi et ullamcorper mollis, magna augue ultrices tortor, nec malesuada quam est.</q>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content area -->
+
+      <!-- content area -->
+      <section id="overview" class="content-section latest-articles position-relative text-center text-light parallax-window" data-parallax="scroll" data-image-src="assets/img/content/bg-1.jpg" data-overlay="5">
+        <div class="container position-relative">
+          <header class="section-header">
+            <h2 class="text-light">Overview</h2>
+            <hr class="w-10 border-warning border-top-2 o-90">
+          </header>
+          <div class="row">
+            <div class="col-md-4 mb-9 mb-md-0">
+              <article class="card article-post bg-dark_A-70 border border-secondary py-6 px-5">
+                <div class="position-relative overflow-hidden br-n bp-c bs-c article-image ar-1_1 w-50 mx-auto rounded-circle mb-4" style="background-image: url(assets/img/avatar/1.jpg)"></div>
+                <figure>
+                  <figcaption>
+                    <h5 class="text-light mt-1 mb-0"><a href="#" class="text-light">Sollicitudin</a></h5>
+                    <hr class="w-10 my-6 my-xl-7 border-warning">
+                    <p>"Mauris tempus enim lorem, nec egestas est fringilla ut. Quisque iaculis dui egestas mi blandit viverra. Nulla luctus vulputate iaculis. Nulla facilisi."</p>
+                    <span class="small text-warning">Marketing Ops</span>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+            <div class="col-md-4 mb-9 mb-md-0">
+              <article class="card article-post bg-dark_A-70 border border-secondary py-6 px-5">
+                <div class="position-relative overflow-hidden br-n bp-c bs-c article-image ar-1_1 w-50 mx-auto rounded-circle mb-4" style="background-image: url(assets/img/avatar/1.jpg)"></div>
+                <figure>
+                  <figcaption>
+                    <h5 class="text-light mt-1 mb-0"><a href="#" class="text-light">Sollicitudin</a></h5>
+                    <hr class="w-10 my-6 my-xl-7 border-warning">
+                    <blockquote>
+                        <p>"Mauris tempus enim lorem, nec egestas est fringilla ut. Quisque iaculis dui egestas mi blandit viverra. Nulla luctus vulputate iaculis. Nulla facilisi."</p>
+                    </blockquote>
+                    <span class="small text-warning">Designer</span>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+            <div class="col-md-4">
+              <article class="card article-post bg-dark_A-70 border border-secondary py-6 px-5">
+                <div class="position-relative overflow-hidden br-n bp-c bs-c article-image ar-1_1 w-50 mx-auto rounded-circle mb-4" style="background-image: url(assets/img/avatar/1.jpg)"></div>
+                <figure>
+                  <figcaption>
+                    <h5 class="text-light mt-1 mb-0"><a href="#" class="text-light">Sollicitudin</a></h5>
+                    <hr class="w-10 my-6 my-xl-7 border-warning">
+                    <blockquote>
+                        <p>"Mauris tempus enim lorem, nec egestas est fringilla ut. Quisque iaculis dui egestas mi blandit viverra. Nulla luctus vulputate iaculis. Nulla facilisi."</p>
+                    </blockquote>
+                    <span class="small text-warning">Software Engineer</span>
+                  </figcaption>
+                </figure>     
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content area -->
+
+      <!-- carousel area -->
+      <section id="gallery" class="section ig-slider-def gradient-lg">
+        <div id="gameCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+          <!-- Indicators -->
+          <div class="po_carousel__wrapper">
+            <ol class="list-unstyled carousel-indicators def po_carousel-indicators">
+              <li data-target="#gameCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#gameCarousel" data-slide-to="1"></li>
+              <li data-target="#gameCarousel" data-slide-to="2"></li>
+            </ol>
+          </div>
+          <!-- carousel items -->
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="ig-img br-n bp-c bs-c" style="background-image: url(assets/img/content/bg-3.jpg);"></div>
+              <div class="ig-sl-gr text-light text-uppercase my-5 my-lg-0">
+                <div class="text-center p-7">
+                  <h3 class="mb-0 mb-md-2">Phasellus viverra</h3>
+                  <p class="mb-0 d-none d-md-block">Nam tincidunt dui eget scelerisque tempor. Nulla facilisi. Praesent aliquam venenatis purus at euismod. Fusce lobortis, eros malesuada volutpat fermentum, lorem magna convallis enim, eget mollis lorem nisi id justo. Suspendisse luctus quam id nulla fermentum porttitor.</p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="ig-img br-n bp-c bs-c" style="background-image: url(assets/img/content/bg-4.jpg);"></div>
+              <div class="ig-sl-gr text-light text-uppercase my-5 my-lg-0">
+                <div class="text-center p-7">
+                  <h3 class="mb-0 mb-md-2">Phasellus viverra</h3>
+                  <p class="mb-0 d-none d-md-block">Nam tincidunt dui eget scelerisque tempor. Nulla facilisi. Praesent aliquam venenatis purus at euismod. Fusce lobortis, eros malesuada volutpat fermentum, lorem magna convallis enim, eget mollis lorem nisi id justo. Suspendisse luctus quam id nulla fermentum porttitor.</p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="ig-img br-n bp-c bs-c" style="background-image: url(assets/img/content/bg-5.jpg);"></div>
+              <div class="ig-sl-gr text-light text-uppercase my-5 my-lg-0">
+                <div class="text-center p-7">
+                  <h3 class="mb-0 mb-md-2">Phasellus viverra</h3>
+                  <p class="mb-0 d-none d-md-block">Nam tincidunt dui eget scelerisque tempor. Nulla facilisi. Praesent aliquam venenatis purus at euismod. Fusce lobortis, eros malesuada volutpat fermentum, lorem magna convallis enim, eget mollis lorem nisi id justo. Suspendisse luctus quam id nulla fermentum porttitor.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.carousel items -->
+          <!-- carousel nav -->
+          <a class="carousel-control-prev" href="#gameCarousel" data-slide="prev"><span class="icon-cl-prev pe-7s-angle-left"></span></a>
+          <a class="carousel-control-next" href="#gameCarousel" data-slide="next"><span class="icon-cl-next pe-7s-angle-right"></span></a>
+          <!-- /.carousel nav -->
+        </div>
+      </section>
+      <!-- /.carousel area -->
+
+      <!-- content area -->
+      <section id="contact" class="content-section latest-articles text-light">
+        <div class="container">
+          <header class="section-header text-center">
+            <h2>Contact Us</h2>
+            <hr class="w-10 border-warning border-top-2 o-90">
+            <p class="lead-1">Proin sed enim eu libero dapibus convallis ut sed felis. Sed luctus ac mi sit amet tincidunt. Nullam in urna dolor. Pellentesque eget venenatis elit, a aliquet velit. Proin metus erat, convallis id sapien sed, porta viverra odio. Mauris ac odio eu libero vehicula pulvinar. Nam ac lacus non turpis fermentum accumsan.</p>
+          </header>
+          <div class="row gutters-y align-items-center">
+            <div class="col-lg-4">
+                <form data-form="contact_form" action="assets/php/contact-form.php" class="input-transparent" method="POST"><div class="alert" role="alert" data-alert="result" style="display: none;"></div>
+                  <div class="form-row">
+                    <div class="form-group col-md-12">
+                      <input class="form-control form-control-lg" type="text" name="name" placeholder="Your Name" required="">
+                    </div>
+                    <div class="form-group col-md-12">
+                      <input class="form-control form-control-lg" type="email" name="email" placeholder="Your Email Address" required="">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <input name="subject" type="text" class="form-control" placeholder="Subject" required="">
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control form-control-lg" rows="4" placeholder="Your Message" name="message" required=""></textarea>
+                  </div>
+                  <button class="btn btn-lg btn-warning" type="submit">Send it over</button>
+              </form>
+            </div>
+            <div class="col-lg-8">
+              <div class="py-13 rounded" data-map="true" data-map-style="custom" data-lat="35.61030245" data-lng="140.11365268" data-marker-lat="35.61030245" data-marker-lng="140.11365268" data-marker-animation="drop" data-marker-title="Office" data-zoom="12"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /.content area -->
+
+    </main>
+
+    <!-- footer -->
+    <footer class="footer footer-dark py-0">
+      <div class="bg-dark py-9">
+        <div class="container">
+          <div class="row gutters-y align-items-center">
+            <div class="col-lg-9">
+              <div class="nav nav-bold-style text-uppercase">
+                <a class="nav-link py-1 pb-sm-0" href="about.html">About</a>
+                <a class="nav-link py-1 pb-sm-0" href="news.html">News</a>
+                <a class="nav-link py-1 pb-sm-0" href="store.html">Store</a>
+                <a class="nav-link py-1 pb-sm-0" href="contact.html">Contact</a>
+              </div>
+            </div>
+            
+            <div class="col-lg-3">
+              <div class="social-buttons lead-1">
+                <a class="social-facebook" href="#"><i class="fab fa-facebook-f"></i></a>
+                <a class="social-twitter" href="#"><i class="fab fa-twitter"></i></a>
+                <a class="social-dribbble" href="#"><i class="fab fa-dribbble"></i></a>
+                <a class="social-instagram" href="#"><i class="fab fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-dark-end py-6 text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <span class="ls-1 fw-500">Copyright © 2020 Wicodus. All Rights Reserved.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- /.footer -->
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- User JS -->
+    <script src="assets/js/scripts.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js" id="_mainJS" data-plugins="load"></script>
+  </body>
+</html>

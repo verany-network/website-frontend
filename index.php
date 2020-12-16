7d2f3ca0ef9b0ca934aba6caefe8dedc7bc24892
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-12-16 18:44:29 +0100

    Updated login/register
    Updated login/register:
    - renamed to sign up and sign in
    - removed social media logins
    - edited form (removed and added content)

--- index.php
+++ index.php
@@ -888,23 +888,15 @@
       <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
         <div class="modal-content bg-dark text-light">
           <div class="modal-header border-secondary">
-            <h5 class="modal-title" id="userLoginTitle">Login</h5>
+            <h5 class="modal-title" id="userLoginTitle">Sign in</h5>
             <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            <div>
-              <div class="text-center my-6"> 
-                <a class="btn btn-circle btn-sm btn-google mr-2" href=""><i class="fab fa-google"></i></a>
-                <a class="btn btn-circle btn-sm btn-facebook mr-2" href=""><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-circle btn-sm btn-twitter" href=""><i class="fab fa-twitter"></i></a>
-              </div>
-              <span class="hr-text small my-6">Or</span>
-            </div>
             <form class="input-transparent">
               <div class="form-group">
-                <input type="text" class="form-control border-secondary" name="username" placeholder="Username">
+                <input type="text" class="form-control border-secondary" name="username" placeholder="Minecraft Username">
               </div>
               <div class="form-group">
                 <input type="password" class="form-control border-secondary" name="password" placeholder="Password">
@@ -949,15 +941,22 @@
               <div class="form-group input-group mb-0">
                 <input type="text" class="form-control" placeholder="Enter Your Code">
                   <div class="input-group-append ml-3">
-                  <button class="btn btn-warning" type="button">Apply</button>
+                  <button class="btn btn-warning" type="button">CHECK</button>
                 </div>
               </div>
               <div class="form-group d-flex justify-content-between">
+                <div class="form-group d-flex justify-content-between">
+                  <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" checked="" id="rememberMeCheck">
+                    <label class="custom-control-label" for="rememberMeCheck">Remember me</label> 
+                  </div>
+                </div>
               </div>
               <div class="form-group mt-6">
                 <button class="btn btn-block btn-warning" type="submit">Sign up</button>
               </div>
             </form>
+            <span class="small">Already have an account? <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#userSignUp" >Create an account</a></span>
           </div>
         </div>
       </div>

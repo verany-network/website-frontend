286abdae8f814e266d1d3d64bd280ab35b12c821
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-12-17 18:15:35 +0100

    Updated page title
    Updated page title:
    - from "-" to "•"

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Verany - Home</title> 
+    <title>Verany • Home</title> 
 
     <!-- CSS -->
     <link href="assets/css/fonts/etline-font.min.css" rel="stylesheet">

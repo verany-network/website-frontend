563922da29fdb42d78830952330ed86c6ca8f8e3
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-12-24 22:18:21 +0100

    Removed support content
    Removed support content:
    - removed search form
    - removed 4 buttons
    - updated icons & button names
    - fixed logo directory

--- support/help-center.php
+++ support/help-center.php
@@ -119,7 +119,8 @@
           <header class="header mb-8">
             <nav aria-label="breadcrumb">
               <ol class="breadcrumb-product breadcrumb breadcrumb-nowrap breadcrumb-angle bg-transparent pl-0 pr-0 mb-0">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="../">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Support</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Help Center</li>
               </ol>
             </nav>
@@ -131,20 +132,12 @@
                   <h2 class="mb-0">Welcome to our Support!</h2>
                   <hr class="w-10 border-top-2 mt-5 mb-6 mx-auto border-warning">
                 </header>
-                <div class="mb-8">
-                  <form class="input-group border-0 input-transparent bg-transparent">
-                    <input class="form-control border border-secondary border-right-0" type="search" placeholder="Search" aria-label="Search">
-                    <div class="input-group-append">
-                      <button class="btn btn-sm btn-warning text-secondary my-0 mx-0" type="submit"><i class="fas fa-search"></i></button>
-                    </div>
-                  </form>
-                </div>
                 <div class="help_m row gutters-y text-center">
                   <div class="col-md-6 col-lg-3">
                     <a href="help-center-article.html">
                       <div class="card help_card">
                         <div class="card-body py-7">
-                          <span class="ti-user lead-7 d-block mb-3"></span>
+                          <span class="fas fa-user lead-7 d-block mb-3"></span>
                           <h5 class="card-title mb-0">Account</h5>
                         </div>
                       </div>
@@ -154,7 +147,17 @@
                     <a href="help-center-article.html">
                       <div class="card help_card">
                         <div class="card-body py-7">
-                          <span class="ti-game lead-7 d-block mb-3"></span>
+                          <span class="fas fa-mail-bulk lead-7 d-block mb-3"></span>
+                          <h5 class="card-title mb-0">Report</h5>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="col-md-6 col-lg-3">
+                    <a href="help-center-article.html">
+                      <div class="card help_card">
+                        <div class="card-body py-7">
+                          <span class="fas fa-gamepad lead-7 d-block mb-3"></span>
                           <h5 class="card-title mb-0">Games</h5>
                         </div>
                       </div>
@@ -164,172 +167,11 @@
                     <a href="help-center-article.html">
                       <div class="card help_card">
                         <div class="card-body py-7">
-                          <span class="ti-wallet lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Payments</h5>
+                          <span class="fas fa-cash-register lead-7 d-block mb-3"></span>
+                          <h5 class="card-title mb-0">Store</h5>
                         </div>
                       </div>
                     </a>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <a href="help-center-article.html">
-                      <div class="card help_card">
-                        <div class="card-body py-7">
-                          <span class="ti-settings lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Settings</h5>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-
-                  <div class="col-md-6 col-lg-3">
-                    <a href="help-center-article.html">
-                      <div class="card help_card">
-                        <div class="card-body py-7">
-                          <span class="ti-email lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Mailboxes</h5>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <a href="help-center-article.html">
-                      <div class="card help_card">
-                        <div class="card-body py-7">
-                          <span class="ti-lock lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Security</h5>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <a href="help-center-article.html">
-                      <div class="card help_card">
-                        <div class="card-body py-7">
-                          <span class="ti-comments lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Community</h5>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <a href="help-center-article.html">
-                      <div class="card help_card">
-                        <div class="card-body py-7">
-                          <span class="ti-mobile lead-7 d-block mb-3"></span>
-                          <h5 class="card-title mb-0">Devices</h5>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <header class="text-center mb-7 mb-md-8">
-                  <h2 class="mb-0">Trending Topics</h2>
-                  <hr class="w-10 border-top-2 mt-5 mb-6 mx-auto border-warning">
-                </header>
-                <div class="row gutters-y">
-                  <div class="col-md-6 col-lg-3">
-                    <div>
-                      <h6>Getting Started</h6>
-                      <ul class="list-unstyled mb-0">
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            How to Begin
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Account Basics
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Editor Basics
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <div>
-                      <h6>Games</h6>
-                      <ul class="list-unstyled mb-0">
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            System files
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            System requirements
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Localization
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <div>
-                      <h6>Mailboxes</h6>
-                      <ul class="list-unstyled mb-0">
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Mailbox Basics
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Google Apps
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Connecting a Mailbox
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-md-6 col-lg-3">
-                    <div>
-                      <h6>Security</h6>
-                      <ul class="list-unstyled mb-0">
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            2-step verification
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Device sync
-                          </a>
-                        </li>
-                        <li>
-                          <a href="help-center-article.html" class="help_trt">
-                            <span class="ti-angle-right text-warning small-6 mr-2"></span>
-                            Connected services
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -346,7 +188,7 @@
       <div class="container">
           <div class="row gutters-y">
               <div class="col-6 col-lg-3">
-                <a href="#" class="logo d-block mb-4"><img src="assets/img/logo.png" alt="Logo" class="logo-dark"></a>
+                <a href="#" class="logo d-block mb-4"><img src="../assets/img/logo.png" alt="Logo" class="logo-dark"></a>
                 <p>The Verany.net network is a network that specializes in innovative pastimes.</p>
                 <div class="social-buttons">
                   <a class="social-mail" href="mailto:contact@verany.net"><i class="far fa-envelope"></i></a>

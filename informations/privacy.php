92c3293da985d373c6c22b2c9c1485431ccf9f6f
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-12-25 17:11:31 +0100

    Removed sign up content

--- informations/privacy.php
+++ informations/privacy.php
@@ -203,51 +203,7 @@
       </div>
     </footer>
     <!-- /.footer -->
-
-    <!-- sign up -->
-    <div class="modal fade" id="userLogin" tabindex="-1" role="dialog" aria-labelledby="userLoginTitle" aria-hidden="true">
-      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
-        <div class="modal-content bg-dark text-light">
-          <div class="modal-header border-secondary">
-            <h5 class="modal-title" id="userLoginTitle">Log in</h5>
-            <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div>
-              <div class="text-center my-6"> 
-                <a class="btn btn-circle btn-sm btn-google mr-2" href=""><i class="fab fa-google"></i></a>
-                <a class="btn btn-circle btn-sm btn-facebook mr-2" href=""><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-circle btn-sm btn-twitter" href=""><i class="fab fa-twitter"></i></a>
-              </div>
-              <span class="hr-text small my-6">Or</span>
-            </div>
-            <form class="input-transparent">
-              <div class="form-group">
-                <input type="text" class="form-control border-secondary" name="username" placeholder="Username">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control border-secondary" name="password" placeholder="Password">
-              </div>
-              <div class="form-group d-flex justify-content-between">
-                <div class="custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" checked="" id="rememberMeCheck">
-                  <label class="custom-control-label" for="rememberMeCheck">Remember me</label>
-                </div>
-                <a class="small-3" href="#">Forgot password?</a>
-              </div>
-              <div class="form-group mt-6">
-                <button class="btn btn-block btn-warning" type="submit">Login</button>
-              </div>
-            </form>
-            <span class="small">Don't have an account? <a href="#">Create an account</a></span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /.sign up -->
-
+    
     <!-- offcanvas-cart -->
     <div id="offcanvas-cart" class="offcanvas-cart offcanvas text-light h-100 r-0 l-auto d-flex flex-column" data-animation="slideRight">
       <div>

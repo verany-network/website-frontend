956665722b032daa23f9c2ebd44b495947ecd382
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2020-12-27 14:51:35 +0100

    Updated builder application contact page

--- apply/builder/contacts.php
+++ apply/builder/contacts.php
@@ -37,7 +37,7 @@
         <div class="row">
           <div class="col-4 col-sm-3 col-md-1 mr-auto">
             <a class="navbar-brand logo" href="#">
-              <img src="../assets/img/logo.png" alt="Logo" class="logo-light mx-auto">
+              <img src="../../assets/img/logo.png" alt="Logo" class="logo-light mx-auto">
             </a>
           </div>
           <div class="nav-items mr-auto col">
@@ -75,7 +75,7 @@
           <div class="col-4 col-sm-8 col-md-8 col-lg-6 col-xl-4 ml-auto text-right">
             <ul class="nav navbar-nav d-none d-sm-inline-flex flex-row">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle small text-capitalize" href="#" id="dropdownUser" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../assets/img/avatar/2.jpg" class="img-xs rounded-circle mr-2" alt="Avatar">NicoVRNY </a>
+                <a class="nav-link dropdown-toggle small text-capitalize" href="#" id="dropdownUser" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="../../assets/img/avatar/2.jpg" class="img-xs rounded-circle mr-2" alt="Avatar">NicoVRNY </a>
                 <div class="dropdown-menu position-absolute" aria-labelledby="dropdownUser">
                   <a class="dropdown-item" href="#"><span class="mr-2"><i class="fas fa-user"></i></span>Profile</a>
                   <a class="dropdown-item" href="#"><span class="mr-2"><i class="fas fa-sign-out-alt"></i></span>Logout</a>
@@ -142,80 +142,33 @@
               <div class="col-lg-8 mb-8 mb-lg-0">
                 <div>
                   <div>
-                    <h4>Billing details</h4>
+                    <h4>Contact Options</h4>
                     <hr class="border-secondary my-4">
                     <div class="mb-9">
-                      <form class="input-transparent">
+                      <form class="input-transparent" autocomplete="off">
                         <div class="form-row">
                           <div class="form-group col-md-6">
-                            <label class="small-1" for="firstName">First name *</label>
-                            <input class="form-control" id="firstName" type="text" name="firstName" required="">
+                            <label class="small-1" for="emailAddress">E-Mail *</label>
+                            <input class="form-control" id="emailAddress" type="email" name="emailAddress" placeholder="max@mustermann.de" required="" autocomplete="off">
                           </div>
                           <div class="form-group col-md-6">
-                            <label class="small-1" for="lastName">Last name *</label>
-                            <input class="form-control" id="lastName" type="text" name="lastName" required="">
+                            <label class="small-1" for="discordName">Discord *</label>
+                            <input class="form-control" id="discordName" type="text" name="discordName" placeholder="MaxMuster#1234" required="" autocomplete="off">
                           </div>
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="country">Country *</label>
-                          <select class="form-control" id="country">
-                            <option>Argentina</option>
-                            <option>Germany</option>
-                            <option>Japan</option>
-                            <option>South Korea</option>
-                            <option>United Kingdom (UK)</option>
-                            <option>United States (US)</option>
-                          </select>
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="billing_address_1">Street address *</label>
-                          <input name="billing_address_1" type="text" class="form-control" required="" placeholder="House number and street name" id="billing_address_1">
-                        </div>
-                        <div class="form-group">
-                          <input name="billing_address_2" type="text" class="form-control" required="" placeholder="Apartment, suite, unit etc. (optional)" id="billing_address_2">
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="billing_city">Town / City *</label>
-                          <input name="billing_city" type="text" class="form-control" required="" id="billing_city">
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="state">State *</label>
-                          <select class="form-control" id="state">
-                            <option>Alabama</option>
-                            <option>Colorado</option>
-                            <option>Louisiana</option>
-                            <option>New York</option>
-                            <option>Ohio</option>
-                            <option>Wyoming</option>
-                          </select>
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="billing_postcode">ZIP *</label>
-                          <input name="billing_postcode" type="text" class="form-control" required="" id="billing_postcode">
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="billing_phone">Phone *</label>
-                          <input name="billing_phone" type="text" class="form-control" required="" id="billing_phone">
-                        </div>
-                        <div class="form-group">
-                          <label class="small-1" for="billing_email">Email address *</label>
-                          <input name="billing_email" type="email" class="form-control" required="" id="billing_email">
-                        </div>
-                      </form>
-                    </div>
-                    <hr class="border-secondary my-7">
-                    <h4 class="mb-6">Shipping details</h4>
-                    <div>
-                      <form class="input-transparent">
-                        <div class="form-group custom-control custom-checkbox">
+                          <div class="form-group col-md-6">
+                            <label class="small-1" for="minecraftName">Minecraft *</label>
+                            <input class="form-control" id="minecraftName" type="text" name="minecraftName" placeholder="MaxMuster" required="" autocomplete="off">
+                          </div>
+                          <div class="form-group col-md-6">
+                            <label class="small-1" for="twitterName">Twitter *</label>
+                            <input class="form-control" id="twitterName" type="text" name="twitterName" placeholder="MaxMuster" required="" autocomplete="off">
+                          </div>
+                          <div class="form-group custom-control custom-checkbox">
                           <input class="custom-control-input" type="checkbox" value="" id="ship_to_different_address">
-                          <label class="custom-control-label" for="ship_to_different_address">
-                            Ship to a different address?
+                            <label class="custom-control-label" for="ship_to_different_address">
+                            I would like to be informed if the status of my application changes.
                           </label>
                         </div>
-                        <div class="form-group">
-                          <label class="small-1" for="order_comments">Order notes (optional)</label>
-                          <textarea class="form-control" placeholder="Notes about your order, e.g. special notes for delivery." id="order_comments" rows="3"></textarea>
                         </div>
                       </form>
                     </div>
@@ -224,42 +177,49 @@
               </div>
               <div class="col-lg-4">
                 <div class="archive border border-secondary rounded">
-                  <div class="bg-secondary px-4 py-1"><span class="fw-600 ls-1 text-uppercase lead-1">Billing Details</span></div>
+                  <div class="bg-secondary px-4 py-1"><span class="fw-600 ls-1 text-uppercase lead-1">Application Overview</span></div>
                   <div class="p-4">
                     <ul class="list-unstyled small mb-0">
                       <li class="my-1">
                         <div class="d-flex small-2 fw-600">
-                          <div>Street</div>
-                          <div class="ml-auto text-warning">3154  Doctors Drive</div>
+                          <div>Advertised as a:</div>
+                          <div class="ml-auto text-success">Builder</div>
                         </div>
                       </li>
                       <li class="my-1">
                         <div class="d-flex small-2 fw-600">
-                          <div>City</div>
-                          <div class="ml-auto">Los Angeles</div>
+                          <div>Desired area(s):</div>
+                          <div class="ml-auto">Terrain</div>
                         </div>
                       </li>
+                      <li><hr class="my-3 border-secondary"></li>
                       <li class="my-1">
-                        <div class="d-flex text-info small-2 fw-600">
-                          <div>State Full</div>
-                          <div class="ml-auto">California</div>
+                        <div class="d-flex small-2 fw-600">
+                          <div>E-Mail:</div>
+                          <div class="ml-auto">kontakt@nicokempe.de</div>
                         </div>
                       </li>
                       <li class="my-1">
                         <div class="d-flex small-2 fw-600">
-                          <div>Zip Code</div>
-                          <div class="ml-auto text-danger">90017</div>
+                          <div>Discord:</div>
+                          <div class="ml-auto">NicoVRNY#8482</div>
                         </div>
                       </li>
                       <li class="my-1">
                         <div class="d-flex small-2 fw-600">
-                          <div>Phone Number</div>
-                          <div class="ml-auto text-warning">310-341-3767</div>
+                          <div>Minecraft:</div>
+                          <div class="ml-auto">NicoVRNY</div>
+                        </div>
+                      </li>
+                      <li class="my-1">
+                        <div class="d-flex small-2 fw-600">
+                          <div>Twitter:</div>
+                          <div class="ml-auto">NicoVRNY</div>
                         </div>
                       </li>
                       <li><hr class="my-3 border-secondary"></li>
                       <li>
-                        <a href="checkout-payment.php" class="btn btn-lg btn-block btn-warning mt-4 px-3">Deliver To This Address</a>
+                        <a href="checkout-payment.php" class="btn btn-lg btn-block btn-light mt-4 px-3">SEND APPLICATION</a>
                       </li>
                     </ul>
                   </div>
@@ -278,7 +238,7 @@
       <div class="container">
           <div class="row gutters-y">
               <div class="col-6 col-lg-3">
-                <a href="#" class="logo d-block mb-4"><img src="assets/img/logo.png" alt="Logo" class="logo-dark"></a>
+                <a href="#" class="logo d-block mb-4"><img src="../../assets/img/logo.png" alt="Logo" class="logo-dark"></a>
                 <p>The Verany.net network is a network that specializes in innovative pastimes.</p>
                 <div class="social-buttons">
                   <a class="social-mail" href="mailto:contact@verany.net"><i class="far fa-envelope"></i></a>
@@ -323,50 +283,6 @@
       </div>
     </footer>
     <!-- /.footer -->
-
-    <!-- sign Up -->
-    <div class="modal fade" id="userLogin" tabindex="-1" role="dialog" aria-labelledby="userLoginTitle" aria-hidden="true">
-      <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
-        <div class="modal-content bg-dark text-light">
-          <div class="modal-header border-secondary">
-            <h5 class="modal-title" id="userLoginTitle">Log in</h5>
-            <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div>
-              <div class="text-center my-6"> 
-                <a class="btn btn-circle btn-sm btn-google mr-2" href=""><i class="fab fa-google"></i></a>
-                <a class="btn btn-circle btn-sm btn-facebook mr-2" href=""><i class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-circle btn-sm btn-twitter" href=""><i class="fab fa-twitter"></i></a>
-              </div>
-              <span class="hr-text small my-6">Or</span>
-            </div>
-            <form class="input-transparent">
-              <div class="form-group">
-                <input type="text" class="form-control border-secondary" name="username" placeholder="Username">
-              </div>
-              <div class="form-group">
-                <input type="password" class="form-control border-secondary" name="password" placeholder="Password">
-              </div>
-              <div class="form-group d-flex justify-content-between">
-                <div class="custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" checked="" id="rememberMeCheck">
-                  <label class="custom-control-label" for="rememberMeCheck">Remember me</label> 
-                </div>
-                <a class="small-3" href="#">Forgot password?</a>
-              </div>
-              <div class="form-group mt-6">
-                <button class="btn btn-block btn-warning" type="submit">Login</button>
-              </div>
-            </form>
-            <span class="small">Don't have an account? <a href="#">Create an account</a></span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /.sign Up -->
 
     <!-- offcanvas-cart -->
     <div id="offcanvas-cart" class="offcanvas-cart offcanvas text-light h-100 r-0 l-auto d-flex flex-column" data-animation="slideRight">
